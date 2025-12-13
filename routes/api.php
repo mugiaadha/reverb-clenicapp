3639fc9fc630f8358f36124327a97ecd16b33ff5
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-12-13 22:33:09 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,5 @@ use App\Models\Message;
 Route::get('/messages', function (Request $request) {
     return Message::orderBy('created_at', 'asc')->get();
 });
+
+Route::post('/chat/send', [\App\Http\Controllers\ChatController::class, 'send']);
