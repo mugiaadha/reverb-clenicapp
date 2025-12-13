1c08eb346d041c5f3b89167445e8b332836c9f82
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-12-13 22:17:47 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,3 @@ Route::get('/', function () {
 // Simple chat routes
 Route::get('/chat', [\App\Http\Controllers\ChatController::class, 'index']);
 Route::post('/chat/send', [\App\Http\Controllers\ChatController::class, 'send']);
-
-// Temporary API endpoint for messages (keeps `/api/messages` working even if
-// RouteServiceProvider for api routes is not present)
-Route::get('/api/messages', function () {
-    return \App\Models\Message::orderBy('created_at', 'asc')->get();
-});
