e33142bb17667b3726e7a44379cbcf14231a8dba
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-12-15 09:39:52 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\QueueController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Message;
@@ -8,6 +10,6 @@ Route::get('/messages', function (Request $request) {
     return Message::orderBy('created_at', 'asc')->get();
 });
 
-Route::post('/chat/send', [\App\Http\Controllers\ChatController::class, 'send']);
+Route::post('/chat/send', [ChatController::class, 'send']);
 // API to trigger queue call (broadcasts via Reverb)
-Route::post('/queue/call', [\App\Http\Controllers\QueueController::class, 'call']);
+Route::post('/queue/call', [QueueController::class, 'call']);
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\QueueController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Simple chat routes
-Route::get('/chat', [\App\Http\Controllers\ChatController::class, 'index']);
-Route::post('/chat/send', [\App\Http\Controllers\ChatController::class, 'send']);
+Route::get('/chat', [ChatController::class, 'index']);
+Route::post('/chat/send', [ChatController::class, 'send']);
 
 // Queue display
-Route::get('/queue', [\App\Http\Controllers\QueueController::class, 'index']);
+Route::get('/queue', [QueueController::class, 'index']);
 // Dynamic channel URL, e.g. /tabaro/queue will set channel='tabaro'
-Route::get('/{channel}/queue', [\App\Http\Controllers\QueueController::class, 'index']);
+Route::get('/{channel}/queue', [QueueController::class, 'index']);
